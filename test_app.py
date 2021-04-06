333692a5aa0d61e8f0f4da573bffd978addecc73
idam fadilah <idamfadilah.9c@gmail.com>
2021-04-06 17:38:18 +0700

    revisi

--- test_app.py
+++ test_app.py
@@ -481,4 +481,3 @@ class TestApp(unittest.TestCase):
         print('result : ')
         print(result)
         print("score : ",t.score(testAttr, testVar))
-TestApp().test_04_idamFadilah_1184063()
