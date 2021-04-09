99bfb0e0ae3c7565f220f3f9ac5056d6d01d5ccb
Alifia Zahra <azahracip@gmail.com>
2021-04-09 09:06:14 +0700

    1184051_Alifia

--- test_app.py
+++ test_app.py
@@ -678,3 +678,14 @@ class TestApp(unittest.TestCase):
         print(result)
         print("Hasilnya : ",t.score(testAttr, testVar))
         self.assertLessEqual(result[0],1)
+        
+    def test_04_AlifiaZahra_1184051(self):  
+        from Chapter03.AlifiaZahra1184051 import preparation, training, testing
+        datasetpath ='Chapter01/dataset/DisneylandReviews.csv'
+        d_train_att, d_train_label, d_test_att, d_test_label = preparation(datasetpath)
+        clf = training(d_train_att, d_train_label)
+        allresult = testing(clf, d_test_att)
+        print('0: Not Satisfied, 1: Satisfied')
+        print('\n hasil testing : ', allresult)
+        print('Score:', clf.score(d_test_att, d_test_label))
+        self.assertLessEqual(allresult[0],1)
